e6310cf8038bb55e9fe157ef3487c3fa76e99e3d
Artem Listopad <listopad@ozzylogik.com>
2022-05-16 12:57:38 +0300

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "server_name" {
+  description = "Name for WebServer"
+  type        = string
+  default     = "test-srv"
+}
+
+
+variable "server_size" {
+  description = "Server Size for WebServer"
+  type        = string
+  default     = "t2.micro"
+}
